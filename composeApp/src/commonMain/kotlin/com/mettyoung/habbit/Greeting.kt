5dc47c7e2644847553f979f2342390a67542e4d8
Emmett Young <mett.young@bytedance.com>
2024-12-29 21:33:50 +0800

    Remove scaffold

--- composeApp/src/commonMain/kotlin/com/mettyoung/habbit/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.mettyoung.habbit
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/mettyoung/habbit/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.mettyoung.habbit
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
